ef09a5821473d12c7e922cbd93e23e98030a1f25
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-07-16 19:21:28 -0700

    updated eslint configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
 import './styles/App.css';
-import App from './components/App'
-import * as serviceWorker from './serviceWorker'
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { BrowserRouter } from 'react-router-dom'
-import { setContext } from 'apollo-link-context'
-import { AUTH_TOKEN } from './constants'
-import { split } from 'apollo-link'
-import { WebSocketLink } from 'apollo-link-ws'
-import { getMainDefinition } from 'apollo-utilities'
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
+import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { AUTH_TOKEN } from './constants';
+import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { SubscriptionClient } from "subscriptions-transport-ws";
 
-const port = process.env.PORT || 4000
+
+const port = process.env.PORT || 4000;
 const httpLink = createHttpLink({
   uri: `http://localhost:${port}`,
   // uri: `https://keepbook-123.herokuapp.com`,
-})
+});
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(AUTH_TOKEN)
+  // eslint-disable-next-line no-undef
+  const token = localStorage.getItem(AUTH_TOKEN);
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : '',
     },
-  }
-})
+  };
+});
 
 const GRAPHQL_ENDPOINT = `ws://localhost:${port}`;
 // const GRAPHQL_ENDPOINT = `wss://keepbook-123.herokuapp.com`;
@@ -39,6 +42,7 @@ const GRAPHQL_ENDPOINT = `ws://localhost:${port}`;
 const wsClient = new SubscriptionClient(GRAPHQL_ENDPOINT, {
   reconnect: true,
   connectionParams: {
+    // eslint-disable-next-line no-undef
     authToken: localStorage.getItem(AUTH_TOKEN),
   },
 });
@@ -57,17 +61,17 @@ const wsLink = new WebSocketLink(wsClient);
 
 const link = split(
   ({ query }) => {
-    const { kind, operation } = getMainDefinition(query)
-    return kind === 'OperationDefinition' && operation === 'subscription'
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
   },
   wsLink,
   authLink.concat(httpLink),
-)
+);
 
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
-})
+});
 
 ReactDOM.render(
   <BrowserRouter>
@@ -76,5 +80,5 @@ ReactDOM.render(
     </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root'),
-)
-serviceWorker.unregister()
+);
+serviceWorker.unregister();
