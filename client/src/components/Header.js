a6db13bdd5532233dd0f4903788d414296044ca5
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-07-17 15:16:23 -0700

    add Account and Account List  component header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
-import { Link , withRouter } from 'react-router-dom'
-import { AUTH_TOKEN } from '../constants'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import logo from '../assets/images/logo.png'
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { AUTH_TOKEN } from '../constants';
+import logo from '../assets/images/logo.png';
 
 const containerStyle = {
-  color:'white'
+  color: 'white',
   // borderBottom: ' 1px solid #00acc1'
 };
 
 const navStyle = {
-  backgroundColor: "#ff8a80",
-  color:'#FFFFFF'
+  backgroundColor: '#ff8a80',
+  color: '#FFFFFF',
   // borderBottom: ' 1px solid #00acc1'
 };
 
@@ -23,14 +23,13 @@ const dropStyle = {
 };
 
 class Header extends Component {
-
   render() {
-    const authToken = localStorage.getItem(AUTH_TOKEN)
+    const authToken = localStorage.getItem(AUTH_TOKEN);
     return (
       <div style={containerStyle}>
         <Navbar style={navStyle}>
-        <Navbar.Brand href="#home">
-          <img
+          <Navbar.Brand href="#home">
+            <img
             alt="logo"
             src={logo}
             width="80"
@@ -38,8 +37,8 @@ class Header extends Component {
           />
           </Navbar.Brand>
           {authToken && (
-            <Nav className="mr-auto" >
-              <NavDropdown title="Payable" id="basic-nav-dropdown" >
+            <Nav className="mr-auto">
+              <NavDropdown title="Payable" id="basic-nav-dropdown">
                 <NavDropdown.Item style={dropStyle} href="/createvendor">Vendor</NavDropdown.Item>
                 <NavDropdown.Item style={dropStyle} href="/vendor">Vendor List</NavDropdown.Item>
                 <NavDropdown.Item style={dropStyle} href="/createbill">Bill</NavDropdown.Item>
@@ -57,6 +56,12 @@ class Header extends Component {
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="/searchinvoices">Search</NavDropdown.Item>
               </NavDropdown>
+              <NavDropdown title="Accounts" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/createaccount">Accounts</NavDropdown.Item>
+                <NavDropdown.Item href="/accountlist">Account List</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/searchinvoices">Search</NavDropdown.Item>
+              </NavDropdown>
               <NavDropdown title="Dashboard" id="basic-nav-dropdown">
                 <NavDropdown.Item href="/chart">Charts</NavDropdown.Item>
                 <NavDropdown.Item href="/report">Reports</NavDropdown.Item>
@@ -65,15 +70,16 @@ class Header extends Component {
               </NavDropdown>
             </Nav>
           )}
-        <div className="flex flex-fixed">
-          {authToken ? (
-            <Button variant="light"
+          <div className="flex flex-fixed">
+            {authToken ? (
+            <Button
+              variant="light"
               className="ml1 pointer black"
               onClick={() => {
-                localStorage.removeItem(AUTH_TOKEN)
-                this.props.history.push(`/login`)
+                localStorage.removeItem(AUTH_TOKEN);
+                this.props.history.push('/login');
               }}
-              >
+            >
               Logout
             </Button>
           ) : (
@@ -81,11 +87,11 @@ class Header extends Component {
               Login
             </Link>
           )}
-        </div>
+          </div>
         </Navbar>
       </div>
     );
   }
 }
 
-export default withRouter(Header)
+export default withRouter(Header);
