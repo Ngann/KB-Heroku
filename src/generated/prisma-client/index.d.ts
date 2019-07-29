dbe007cb4d4216bec0dbc0f2e03a9f18701b02c5
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-07-29 10:37:33 -0700

    remove AccountCategory datatype

--- src/generated/prisma-client/index.d.ts
+++ src/generated/prisma-client/index.d.ts
@@ -349,12 +349,7 @@ export type Payment = "CreditCard" | "Bank" | "Cash" | "EFT" | "Checks";
 
 export type Status = "Active" | "Inactive" | "Open" | "Close";
 
-export type AccountCategory =
-  | "Assets"
-  | "Liability"
-  | "Liablity"
-  | "Revenue"
-  | "Expense";
+export type AccountCategory = "Assets" | "Liability" | "Revenue" | "Expense";
 
 export type AccountType = "Debit" | "Credit";
 
