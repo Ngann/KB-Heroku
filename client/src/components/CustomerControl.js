5f23296c0d37ff6ac5b0e1b6d966d2bcc477f907
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-08 16:57:07 -0700

    update button and state for new components

--- client/src/components/CustomerControl.js
+++ client/src/components/CustomerControl.js
@@ -1,14 +1,57 @@
 import React, { Component} from 'react';
 import CreateCustomerTwo from './CreateCustomerTwo';
 import CustomerListTwo from './CustomerListTwo';
+import { Button, ButtonGroup } from "react-bootstrap";
+import SearchBills from './SearchBills';
 
-
+const buttonStyle = {
+    padding: "2%",
+    backgroundColor: "#FDFFFC",
+    border: "1px solid lightgrey"
+  };
 
 class CustomerControl extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            customerForm: false,
+            search: false,
+            addButton: true,
+            searchButton: true
+        }
+
+        this.openCustomerForm = this.openCustomerForm.bind(this)
+        this.openSearch = this.openSearch.bind(this)
+    }
+
+openCustomerForm() {
+    this.setState({
+        customerForm: !this.state.customerForm,
+        addButton: !this.state.addButton
+    })
+}
+
+openSearch() {
+    this.setState({
+        search: !this.state.search,
+        searchButton: !this.state.searchButton
+    })
+}
+
     render(){
+        // console.log(this.state)
         return(
-            <div>Main Control
-                <CreateCustomerTwo/>
+            <div>
+                <div className="d-flex flex-column" style={buttonStyle}>
+                    <ButtonGroup className="mt-3">
+                        <Button size="md" variant="outline-success" onClick={this.openCustomerForm}>{this.state.addButton ? 'Add Customer': 'Close Customer Form' }</Button> 
+                        <Button size="md" variant="outline-success"> Invoice Payments</Button>
+                        <Button size="md" variant="outline-success"> Reports</Button>
+                        <Button size="md" variant="outline-success" onClick={this.openSearch}> {this.state.searchButton ? 'Search': 'Close Search'}</Button>
+                    </ButtonGroup>
+                </div>
+                 {this.state.customerForm ? <CreateCustomerTwo/> : null }
+                 {this.state.search ? <SearchBills/> : null }
                 <CustomerListTwo/>
             </div>
         )
--- client/src/components/CustomerListTwo.js
+++ client/src/components/CustomerListTwo.js
@@ -1,56 +1,155 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from "react";
 import { ButtonGroup, Table, Button, Modal, Form, Col } from "react-bootstrap";
 
 const containerStyle = {
-    marginTop: "10%",
-    backgroundColor: "#FDFFFC"
-  };
+  marginTop: "10%",
+  backgroundColor: "#FDFFFC"
+};
 
 let listOfCustomers = [
-    {"name": "Customer One", "contact": "Contact One", "address": "AddressOne", "city": "cityOne", "state": "stateOne"},
-    {"name": "Customer Two", "contact": "Contact Two", "address": "AddressTwo", "city": "cityTwo", "state": "stateTwo"},
-    {"name": "Customer Three", "contact": "Contact Three", "address": "AddressThree", "city": "cityThree", "state": "stateThree"},
-    {"name": "Customer Four", "contact": "Contact Four", "address": "AddressFour", "city": "CityFour", "state": "stateFour"},
-]
+  {
+    name: "Customer One",
+    contact: "Contact One",
+    address: "Address One",
+    city: "City One",
+    state: "State One"
+  },
+  {
+    name: "Customer Two",
+    contact: "Contact Two",
+    address: "Address Two",
+    city: "City Two",
+    state: "State Two"
+  },
+  {
+    name: "Customer Three",
+    contact: "Contact Three",
+    address: "Address Three",
+    city: "City Three",
+    state: "State Three"
+  },
+  {
+    name: "Customer Four",
+    contact: "Contact Four",
+    address: "Address Four",
+    city: "City Four",
+    state: "State Four"
+  }
+];
 class CustomerListTwo extends Component {
-    render(){
-        return(
-            <div className="container" style={containerStyle}>
-              <Table striped hover size="sm">
-                <thead>
-                  <tr>
-                    <th>Customer Name</th>
-                    <th>Contact</th>
-                    <th>Address</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Action</th>
-                  </tr>
-                  </thead>
-                  <Fragment >
-                    <tbody>
-                     
-                    { listOfCustomers.map((customer) =>
-                     <tr>
-                        <td>{customer.name}</td>
-                        <td>{customer.contact}</td>
-                        <td>{customer.address}</td>
-                        <td>{customer.city}</td>
-                        <td>{customer.state}</td>
-                        <td>
-                        <ButtonGroup size="sm">
-                            <Button variant="outline-secondary"> Edit</Button>
-                            <Button variant="outline-secondary"> Delete</Button>
-                        </ButtonGroup>
-                        </td>
-                        </tr>
-                        )}
-                    </tbody>
-                  </Fragment>
-              </Table>
-            </div>
-        )
+    constructor(props){
+        super(props)
+        this.state = {
+            show: false
+        }
+
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
+
+    handleClose() {
+        this.setState({ show: false});
+    }
+        
+    handleShow() {
+    this.setState({ show: true});
+    }
+
+  render() {
+    return (
+      <div className="container" style={containerStyle}>
+        <Table striped hover size="sm">
+          <thead>
+            <tr>
+              <th>Customer Name</th>
+              <th>Contact</th>
+              <th>Address</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <Fragment>
+            <tbody>
+              {listOfCustomers.map((customer,i) => (
+                <tr key={i}>
+                  <td>{customer.name}</td>
+                  <td>{customer.contact}</td>
+                  <td>{customer.address}</td>
+                  <td>{customer.city}</td>
+                  <td>{customer.state}</td>
+                  <td>
+                    <ButtonGroup size="sm">
+                      <Button variant="outline-secondary" onClick={this.handleShow}> Edit</Button>
+                      <Button variant="outline-secondary"> Delete</Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Fragment>
+        </Table>
+
+        <Modal
+         show={this.state.show}
+         onHide={this.handleClose}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header >
+            <Modal.Title>Customer</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Row>
+              <Col>
+                <Form.Label>Customer Name</Form.Label>
+                <Form.Control className="mb2" type="text" />
+              </Col>
+              <Col>
+                <Form.Label>Phone</Form.Label>
+                <Form.Control className="mb2" type="text" />
+              </Col>
+            </Form.Row>
+
+            <Form.Group controlId="formBasicContact">
+              <Form.Label>Address</Form.Label>
+              <Form.Control className="mb2" type="text" />
+            </Form.Group>
+            <Form.Group controlId="formBasicContact">
+              <Form.Label>Address 2</Form.Label>
+              <Form.Control className="mb2" type="text" />
+            </Form.Group>
+            <Form.Row>
+              <Col>
+                <Form.Group controlId="formBasicContact">
+                  <Form.Label>City</Form.Label>
+                  <Form.Control className="mb2" type="text" />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formBasicContact">
+                  <Form.Label>State</Form.Label>
+                  <Form.Control className="mb2" type="text" />
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group controlId="formBasicContact">
+                  <Form.Label>Zip</Form.Label>
+                  <Form.Control className="mb2" type="text" />
+                </Form.Group>
+              </Col>
+            </Form.Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>Close</Button>
+            <Button>Save Changes</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default CustomerListTwo
+export default CustomerListTwo;
